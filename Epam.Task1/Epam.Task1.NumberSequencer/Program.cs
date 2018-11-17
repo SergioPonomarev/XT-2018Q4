37f5a72b8d1b8111f04dfb15b13d717ce0350331
Tahmasib <sergiopono@gmail.com>
2018-11-17 21:08:54 +0400

    NumberSequencer method implementation

--- Epam.Task1/Epam.Task1.NumberSequencer/Program.cs
+++ Epam.Task1/Epam.Task1.NumberSequencer/Program.cs
@@ -6,7 +6,18 @@ namespace Epam.Task1.NumberSequencer
     {
         static void Main()
         {
+            Console.WriteLine(NumberSequencer(7));
+        }
 
+        public static string NumberSequencer(int n)
+        {
+            string result = string.Empty;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == n) result += i.ToString();
+                else result += i.ToString() + ", ";
+            }
+            return result;
         }
     }
 }
