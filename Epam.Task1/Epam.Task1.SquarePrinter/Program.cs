d10686b7a2d3c6a197af36aac4159cc86b0044eb
Tahmasib <sergiopono@gmail.com>
2018-11-18 11:01:04 +0400

    SquarePrinter method implementation

--- Epam.Task1/Epam.Task1.SquarePrinter/Program.cs
+++ Epam.Task1/Epam.Task1.SquarePrinter/Program.cs
@@ -6,7 +6,27 @@ namespace Epam.Task1.SquarePrinter
     {
         static void Main()
         {
+            SquarePrinter(7);
+            Console.WriteLine();
+            SquarePrinter(5);
+            Console.WriteLine();
+            SquarePrinter(11);
+        }
 
+        public static void SquarePrinter(int n)
+        {
+            char ch = '*';
+            char space = ' ';
+            string halfString = new string(ch, n / 2);
+            string fullString = new string(ch, n);
+            for (int i = 0; i < n; i++)
+            {
+                if (i == n / 2)
+                {
+                    Console.WriteLine(halfString + space + halfString);
+                }
+                else Console.WriteLine(fullString);
+            }
         }
     }
 }
