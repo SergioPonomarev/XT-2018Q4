249655cce518bddfe7b1115e192f8bd4e45552d8
Tahmasib <sergiopono@gmail.com>
2018-11-18 10:46:52 +0400

    CheckSimplicity method implementation

--- Epam.Task1/Epam.Task1.CheckSimplicity/Program.cs
+++ Epam.Task1/Epam.Task1.CheckSimplicity/Program.cs
@@ -6,6 +6,26 @@ namespace Epam.Task1.CheckSimplicity
     {
         static void Main()
         {
+            Console.WriteLine("1 - " + CheckSimplicity(1).ToString());
+            Console.WriteLine("2 - " + CheckSimplicity(2).ToString());
+            Console.WriteLine("3 - " + CheckSimplicity(3).ToString());
+            Console.WriteLine("7 - " + CheckSimplicity(7).ToString());
+            Console.WriteLine("9 - " + CheckSimplicity(9).ToString());
+            Console.WriteLine("55 - " + CheckSimplicity(55).ToString());
+            Console.WriteLine("71 - " + CheckSimplicity(71).ToString());
+        }
+
+        public static bool CheckSimplicity(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i <= n / i; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
