1a42845a31333d08afa35fce066124eb9156df7b
Tahmasib <sergiopono@gmail.com>
2018-11-20 01:32:57 +0400

    Task1 Square exercise update.
    1. Console UI was added.
    2. All string were removed in Square method. Console.WriteLine implementation.

--- Epam.Task1/Epam.Task1.SquarePrinter/Program.cs
+++ Epam.Task1/Epam.Task1.SquarePrinter/Program.cs
@@ -6,22 +6,62 @@ namespace Epam.Task1.Square
     {
         static void Main()
         {
+            Console.WriteLine("Greatings! You are using " +
+                              "The Square Printer Programm!");
+            Console.WriteLine();
 
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Please, enter a size of square to print: ");
+                    bool check = int.TryParse(Console.ReadLine(), out int size);
+                    Console.WriteLine();
+
+                    if (!check || (size % 2 == 0))
+                    {
+                        throw new ArgumentException("The entered value must be "
+                                                    + "an odd natural number.");
+                    }
+
+                    Square(size);
+                    return;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
         }
 
-        public static void Square(int n)
+        public static void Square(int size)
         {
-            char ch = '*';
-            char space = ' ';
-            string halfString = new string(ch, n / 2);
-            string fullString = new string(ch, n);
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < size; i++)
             {
-                if (i == n / 2)
+                if (i == size / 2)
                 {
-                    Console.WriteLine(halfString + space + halfString);
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (j == size / 2)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                    Console.WriteLine();
                 }
-                else Console.WriteLine(fullString);
+                else
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
             }
         }
     }
