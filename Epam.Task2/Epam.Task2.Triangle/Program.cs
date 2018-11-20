7b1fa5f3b9bd1b9d7350b12ef632b81649d237d2
Tahmasib <sergiopono@gmail.com>
2018-11-20 12:02:13 +0400

    Task2 Triangle exercise implementation.

---
+++ Epam.Task2/Epam.Task2.Triangle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Epam.Task2.Triangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Greatings! You are using "
+                             + "The Triangle Printer Programm!");
+            Console.WriteLine();
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Please, enter a number of strings "
+                                      + "to print a triangle: ");
+                    bool check = int.TryParse(Console.ReadLine(), out int number);
+                    Console.WriteLine();
+
+                    if (!check || number < 1)
+                    {
+                        throw new ArgumentException("The entered value must "
+                                                    + "be a natural number.");
+                    }
+
+                    Triangle(number);
+                    return;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static void Triangle(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
