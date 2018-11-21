2dae3a2a628a259f648b3aad7ba21feeebb92989
Tahmasib <sergiopono@gmail.com>
2018-11-21 13:37:12 +0400

    Task2 Sum Of Numbers exercise implementation.

---
+++ Epam.Task2/Epam.Task2.SumOfNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Epam.Task2.SumOfNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("The sum of all multiples of 3 or 5 below 1000 is: ");
+            SumOfNumbers();
+        }
+
+        public static void SumOfNumbers()
+        {
+            int sum = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
