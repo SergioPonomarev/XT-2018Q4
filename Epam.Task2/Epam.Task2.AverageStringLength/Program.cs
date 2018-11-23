e8f7372e35f8a8ef26f7d5f54f87a87bde0c2008
Tahmasib <sergiopono@gmail.com>
2018-11-23 22:14:24 +0400

    Taks2 Average String Length exercise implementation.

---
+++ Epam.Task2/Epam.Task2.AverageStringLength/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace Epam.Task2.AverageStringLength
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Greatings! You are using The Average "
+                             + "String Length Calculator Program!");
+            Console.WriteLine();
+
+            Console.WriteLine("Please, introduce the string:");
+            string inputString = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("The string entered: ");
+            Console.WriteLine(inputString);
+            Console.WriteLine();
+
+            string convertedString = StringConverter(inputString);
+
+            string[] words = 
+                convertedString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("The average word length in the entered "
+                              + "string is: {0:F2}",
+                              GetAverageLength(words));
+        }
+
+        private static double GetAverageLength(string[] arr)
+        {
+            int charCount = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                charCount += arr[i].Length;
+            }
+
+            return (double)charCount / arr.Length;
+        }
+
+        private static string StringConverter(string str)
+        {
+            StringBuilder sb = new StringBuilder(str);
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (char.IsDigit(sb[i]) ||
+                   char.IsNumber(sb[i]) ||
+                   char.IsPunctuation(sb[i]))
+                {
+                    sb.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            string tempStr = sb.ToString();
+            return tempStr;
+        }
+    }
+}
