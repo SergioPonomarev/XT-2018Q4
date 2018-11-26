033ef38b6d8886ca1413d0b833852051e84b3d54
Sergey Ponomarev <sergiopono@gmail.com>
2018-11-27 00:15:20 +0400

    Task2 Char Doubler style refactoring conform to stylecop.

--- Epam.Task2/Epam.Task2.CharDoubler/Program.cs
+++ Epam.Task2/Epam.Task2.CharDoubler/Program.cs
@@ -3,21 +3,18 @@ using System.Text;
 
 namespace Epam.Task2.CharDoubler
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
-            Console.WriteLine("Greatings! You are using The Char "
-                             + "Doubler Program!");
+            Console.WriteLine("Greatings! You are using The Char Doubler Program!");
             Console.WriteLine();
 
             Console.WriteLine("Please, enter the main string:");
             string mainString = Console.ReadLine();
             Console.WriteLine();
 
-            Console.WriteLine("Please, enter the additional string "
-                              + $"which characters will double{Environment.NewLine}"
-                              + "same characters in the main string:");
+            Console.WriteLine($"Please, enter the additional string which characters will double{Environment.NewLine}same characters in the main string:");
             string additionalString = Console.ReadLine();
             Console.WriteLine();
 
@@ -27,8 +24,7 @@ namespace Epam.Task2.CharDoubler
             Console.WriteLine(result);
         }
 
-        private static string CharDoubler(string mainString,
-                                          string additionalString)
+        private static string CharDoubler(string mainString, string additionalString)
         {
             StringBuilder sb = new StringBuilder(mainString);
 
