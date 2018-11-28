ef98bf583221059b59a444730d7f97cceb6198f3
Sergey Ponomarev <sergiopono@gmail.com>
2018-11-29 00:40:33 +0400

    Task3 Round exercise Round class implementation.

---
+++ Epam.Task3/Epam.Task3.Round/Round.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Epam.Task3.Round
+{
+    public class Round
+    {
+        private int radius;
+
+        public Round(int radius, int centerX, int centerY)
+        {
+            this.Radius = radius;
+            this.Center = new Point(centerX, centerY);
+        }
+
+        public Point Center { get; set; }
+
+        public int Radius
+        {
+            get => this.radius;
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("The radius of the round must be more than 0.");
+                }
+
+                this.radius = value;
+            }
+        }
+
+        public double Circumference
+        {
+            get
+            {
+                return 2 * Math.PI * this.radius;
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                return Math.PI * this.radius * this.radius;
+            }
+        }
+    }
+}
