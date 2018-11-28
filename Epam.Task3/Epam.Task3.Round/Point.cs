7fb6b90bf85c4e7718a3348d2aaa96ee929914db
Sergey Ponomarev <sergiopono@gmail.com>
2018-11-29 00:40:01 +0400

    Task3 Round exercise Point class implementation.

---
+++ Epam.Task3/Epam.Task3.Round/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Epam.Task3.Round
+{
+    public class Point
+    {
+        public Point()
+        {
+        }
+
+        public Point(int value) : this(value, value)
+        {
+        }
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+}
