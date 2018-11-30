cfe2001f9e0e06149e19d180c71661ff3eda3da4
Sergio Ponomarev <sergiopono@gmail.com>
2018-11-30 23:58:20 +0400

    Task3 User exercise Program class implementation (User class using program).

--- Epam.Task3/Epam.Task3.UserProgram/Program.cs
+++ Epam.Task3/Epam.Task3.UserProgram/Program.cs
@@ -1,15 +1,138 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Epam.Task3.UserProgram
+namespace Epam.Task3.User
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static bool surnameCheck;
+        private static bool nameCheck;
+        private static bool patronymicCheck;
+        private static bool birthdayCheck;
+        private static bool check;
+
+        private static void Main()
         {
+            User user = new User();
+
+            Console.WriteLine("Greetings! You are using The User Creating Program!");
+            Console.WriteLine();
+
+            while (!surnameCheck)
+            {
+                try
+                {
+                    Console.Write("Please, enter the surname: ");
+                    string surname = Console.ReadLine();
+
+                    user.Surname = surname;
+
+                    surnameCheck = true;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            while (!nameCheck)
+            {
+                try
+                {
+                    Console.Write("Please, enter the name: ");
+                    string name = Console.ReadLine();
+
+                    user.Name = name;
+
+                    nameCheck = true;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            while (!patronymicCheck)
+            {
+                try
+                {
+                    Console.Write("Please, enter the patronymic. In case of absence of the patronymic, please enter white space: ");
+                    string patronymic = Console.ReadLine();
+
+                    user.Patronymic = patronymic;
+
+                    patronymicCheck = true;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            while (!birthdayCheck)
+            {
+                try
+                {
+                    Console.Write("Please, enter the year of birth in format yyyy: ");
+                    check = int.TryParse(Console.ReadLine(), out int year);
+                    CheckValue(check);
+
+                    Console.Write("Please, enter the month of birth in format mm: ");
+                    check = int.TryParse(Console.ReadLine(), out int month);
+                    CheckValue(check);
+
+                    Console.Write("Please, enter the day of birth in format dd: ");
+                    check = int.TryParse(Console.ReadLine(), out int day);
+                    CheckValue(check);
+
+                    DateTime birthday = new DateTime(year, month, day);
+
+                    user.Birthday = birthday;
+
+                    birthdayCheck = true;
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("New user is created:");
+            Console.WriteLine($"\t- surname is: {user.Surname};");
+            Console.WriteLine($"\t- name is: {user.Name};");
+            Console.WriteLine($"\t- patronymic is: {user.Patronymic};");
+
+            string birth = user.Birthday.ToShortDateString();
+            Console.WriteLine($"\t- date of birth is: {birth};");
+
+            string age = user.Age.ToString();
+            Console.WriteLine($"\t- age is: {age}.");
+        }
+
+        private static bool CheckValue(bool check)
+        {
+            if (!check)
+            {
+                throw new ArgumentException("The value must be an integer.");
+            }
+
+            return true;
         }
     }
 }
