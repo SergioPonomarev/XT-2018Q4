e91af316eb43969a9d9140ddc4d4ef2ee0eb1875
Sergio Ponomarev <sergiopono@gmail.com>
2018-12-05 16:53:39 +0400

    Task3 Game Mob classes implementation.

---
+++ Epam.Task3/Epam.Task3.Game/Characters/Wolf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Epam.Task3.Game
+{
+    public class Wolf : Character
+    {
+        public Wolf(int maxHealth, int strength, int stamina, int agility, Position position) : base(maxHealth, strength, stamina, agility, position)
+        {
+        }
+
+        public override IDamageable DoDamage(IDamageable target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IDamageable GetDamage(int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move(Field field, IMovable movable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
