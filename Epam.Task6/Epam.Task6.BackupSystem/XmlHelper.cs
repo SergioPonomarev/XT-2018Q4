5ad4147aea668b3ba163942c48c672d3be96d494
Sergio Ponomarev <sergiopono@gmail.com>
2018-12-25 13:32:59 +0400

    Task6 BackupSystem XmlHelper class implementation.

---
+++ Epam.Task6/Epam.Task6.BackupSystem/XmlHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Epam.Task6.BackupSystem
+{
+    public static class XmlHelper
+    {
+        private static XmlSerializer serializer;
+
+        static XmlHelper()
+        {
+            serializer = new XmlSerializer(typeof(List<LogDao>));
+        }
+
+        public static void SaveToXml(List<LogDao> logDaos, FileStream fs)
+        {
+            if (logDaos != null)
+            {
+                fs.SetLength(0);
+
+                serializer.Serialize(fs, logDaos);
+            }
+        }
+
+        public static List<LogDao> GetAll(FileStream fs)
+        {
+            if (fs.Length > 0)
+            {
+                return (List<LogDao>)serializer.Deserialize(fs);
+            }
+
+            return new List<LogDao>();
+        }
+
+        public static List<LogDao> GetAll(FileStream fs, out int maxIndex, int capacityIfCreate = 1)
+        {
+            List<LogDao> logDaos;
+
+            if (fs.Length > 0)
+            {
+                logDaos = (List<LogDao>)serializer.Deserialize(fs);
+
+                maxIndex = logDaos.Max(x => x.FileIndex);
+            }
+            else
+            {
+                logDaos = new List<LogDao>(capacityIfCreate);
+
+                maxIndex = 0;
+            }
+
+            return logDaos;
+        }
+
+        public static List<LogDao> GetNeeded(DateTime dateForRollback, FileStream fs)
+        {
+            List<LogDao> orderedAllLogDaos = GetAll(fs)
+                                            .OrderByDescending(x => x.Date)
+                                            .ToList();
+
+            DateTime startDate = orderedAllLogDaos.Last().Date;
+
+            dateForRollback = startDate < dateForRollback
+                ? dateForRollback
+                : startDate;
+
+            var grouping = orderedAllLogDaos.GroupBy(x => x.Path);
+
+            List<LogDao> actualLogDaos = new List<LogDao>(grouping.Count());
+
+            foreach (IGrouping<string, LogDao> groupByPath in grouping)
+            {
+                LogDao logDao = groupByPath.FirstOrDefault(x => x.Date <= dateForRollback);
+
+                if (logDao != null && logDao.ChangeType != WatcherChangeTypes.Deleted)
+                {
+                    actualLogDaos.Add(logDao);
+                }
+            }
+
+            return actualLogDaos;
+        }
+    }
+}
