7c241cef6b91e28a558f675465ad5371ef80d6e0
Sergio Ponomarev <sergiopono@gmail.com>
2018-12-25 12:29:49 +0400

    Task6 BackupSystem Config class implementation.

---
+++ Epam.Task6/Epam.Task6.BackupSystem/Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Epam.Task6.BackupSystem
+{
+    public static class Config
+    {
+        static Config()
+        {
+            BackupDirectory = ConfigurationManager.AppSettings["backupDirectory"];
+            WatchingDirectory = ConfigurationManager.AppSettings["watchingDirectory"];
+            LogFileName = ConfigurationManager.AppSettings["logFile"];
+            FilesInWork = new List<KeyValuePair<string, string>>();
+        }
+
+        public static string DateFormat => "yyyy.MM.dd_HH-mm-ss";
+
+        public static string BackupDirectory { get; set; }
+
+        public static string WatchingDirectory { get; set; }
+
+        public static string LogFileName { get; set; }
+
+        public static Dictionary<string, string> CurrentFiles { get; set; }
+
+        public static List<KeyValuePair<string, string>> FilesInWork { get; set; }
+    }
+}
