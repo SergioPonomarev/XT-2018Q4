7dcd1a86aeaa4671ad30b94fa7cfc752fa5542e2
Sergio Ponomarev <sergiopono@gmail.com>
2018-12-25 14:31:30 +0400

    Task6 BackupSystem WatcherEvents class implementation.

---
+++ Epam.Task6/Epam.Task6.BackupSystem/WatcherEvents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Epam.Task6.BackupSystem
+{
+    public static class WatcherEvents
+    {
+        public static void OnDirectoryDelete(object sender, FileSystemEventArgs e)
+        {
+            WatchingLogic.DirectoryDelete(e.FullPath);
+        }
+
+        public static void OnDirectoryRename(object sender, RenamedEventArgs e)
+        {
+            WatchingLogic.RecursiveDirectoryRename(e.FullPath, e.OldFullPath);
+        }
+
+        public static void OnFileChange(object sender, FileSystemEventArgs e)
+        {
+            WatchingLogic.Change(e.FullPath, e.ChangeType);
+        }
+
+        public static void OnFileRename(object sender, RenamedEventArgs e)
+        {
+            WatchingLogic.Change(e.FullPath, WatcherChangeTypes.Created);
+
+            WatchingLogic.Change(e.OldFullPath, WatcherChangeTypes.Deleted);
+        }
+    }
+}
