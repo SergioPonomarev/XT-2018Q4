de487ce707c35f27749a94ce3ec9f7cc0301cfaa
Sergio Ponomarev <sergiopono@gmail.com>
2018-12-25 12:21:48 +0400

    Task6 BackupSystem LogDao class implementation.

---
+++ Epam.Task6/Epam.Task6.BackupSystem/LogDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Epam.Task6.BackupSystem
+{
+    [Serializable]
+    public class LogDao
+    {
+        public LogDao()
+        {
+        }
+
+        public LogDao(int fileIndex, 
+                      string path, 
+                      DateTime date, 
+                      WatcherChangeTypes changeType)
+        {
+            this.FileIndex = fileIndex;
+            this.Path = path;
+            this.Date = date;
+            this.ChangeType = changeType;
+        }
+
+        public int FileIndex { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Path { get; set; }
+
+        public WatcherChangeTypes ChangeType { get; set; }
+    }
+}
---
+++ Epam.Task6/Epam.Task6.BackupSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task6.BackupSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
