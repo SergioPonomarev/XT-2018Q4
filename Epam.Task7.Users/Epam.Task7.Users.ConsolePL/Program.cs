ebd8afdcb1cc24357595e3278e2213371dbbd38b
Sergio Ponomarev <sergiopono@gmail.com>
2019-01-04 17:48:18 +0400

    Task7 Users Entities User implementation.

--- Epam.Task7.Users/Epam.Task7.Users.ConsolePL/Program.cs
+++ Epam.Task7.Users/Epam.Task7.Users.ConsolePL/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Epam.Task7.Users.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ Epam.Task7.Users/Epam.Task7.Users.Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task7.Users.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return AgeCalculating();
+            }
+        }
+
+        private int AgeCalculating()
+        {
+            DateTime current = DateTime.Now;
+            int a = (((this.DateOfBirth.Year * 100) + this.DateOfBirth.Month) * 100) + this.DateOfBirth.Day;
+            int b = (((current.Year * 100) + current.Month) * 100) + current.Day;
+            int age = (b - a) / 10000;
+            return age;
+        }
+    }
+}
