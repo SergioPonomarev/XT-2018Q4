eefc4f86e3819324938460eff1b0d4d201d9de92
Sergio Ponomarev <sergiopono@gmail.com>
2019-01-04 17:55:24 +0400

    Task7 Users BLL Interfaces implementation.

---
+++ Epam.Task7.Users/Epam.Task7.Users.BLL.Interfaces/IUserLogic.cs
@@ -0,0 +1,20 @@
+﻿using Epam.Task7.Users.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task7.Users.BLL.Interfaces
+{
+    public interface IUserLogic
+    {
+        IEnumerable<User> GetAll();
+
+        bool Create(string userName, string userDateOfBirth);
+
+        bool Delete(int id);
+
+        bool DeleteAll();
+    }
+}
