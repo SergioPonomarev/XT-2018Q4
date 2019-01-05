7ad000caa72de8a82c9a1ccff4e9f7cbab05df62
Sergio Ponomarev <sergiopono@gmail.com>
2019-01-05 22:10:10 +0400

    Task7 Users CacheLogic instance added to UsersLogic.

--- Epam.Task7.Users/Epam.Task7.Users.BLL/UsersLogic.cs
+++ Epam.Task7.Users/Epam.Task7.Users.BLL/UsersLogic.cs
@@ -12,13 +12,17 @@ namespace Epam.Task7.Users.BLL
 {
     public class UsersLogic : IUsersLogic
     {
+        private const string ALL_USERS_CACHE_KEY = "GetAllUsers";
+
         private readonly IUsersDao usersDao;
+        private readonly ICacheLogic cacheLogic;
 
         private const string DateFormat = "yyyy-MM-dd";
 
-        public UsersLogic(IUsersDao usersDao)
+        public UsersLogic(IUsersDao usersDao, ICacheLogic cacheLogic)
         {
             this.usersDao = usersDao;
+            this.cacheLogic = cacheLogic;
         }
 
         public void Add(string userName, string userDateOfBirth)
@@ -59,6 +63,7 @@ namespace Epam.Task7.Users.BLL
 
             try
             {
+                cacheLogic.Delete(ALL_USERS_CACHE_KEY);
                 usersDao.Add(user);
             }
             catch
@@ -69,17 +74,30 @@ namespace Epam.Task7.Users.BLL
 
         public bool Remove(int id)
         {
+            cacheLogic.Delete(ALL_USERS_CACHE_KEY);
             return usersDao.Remove(id);
         }
 
         public bool RemoveAll()
         {
+            cacheLogic.Delete(ALL_USERS_CACHE_KEY);
             return usersDao.RemoveAll();
         }
 
         public IEnumerable<User> GetAll()
         {
-            return usersDao.GetAll().ToArray();
+            var cacheResult = cacheLogic.Get<IEnumerable<User>>(ALL_USERS_CACHE_KEY);
+
+            if (cacheResult == null)
+            {
+                var result = usersDao.GetAll().ToArray();
+                cacheLogic.Add(ALL_USERS_CACHE_KEY, result);
+                Console.WriteLine("From dao");
+                return result;
+            }
+
+            Console.WriteLine("From cache");
+            return cacheResult;
         }
     }
 }
