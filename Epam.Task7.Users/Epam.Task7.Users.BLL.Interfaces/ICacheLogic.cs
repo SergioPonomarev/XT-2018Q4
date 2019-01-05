647473edc459c03b9b4ddf0058ad2ac312bad880
Sergio Ponomarev <sergiopono@gmail.com>
2019-01-05 21:43:13 +0400

    Task7 Users ICacheLogic interface implementation.

---
+++ Epam.Task7.Users/Epam.Task7.Users.BLL.Interfaces/ICacheLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task7.Users.BLL.Interfaces
+{
+    public interface ICacheLogic
+    {
+        bool Add<T>(string key, T value);
+
+        T Get<T>(string key) where T : class;
+
+        bool Delete(string key);
+    }
+}
