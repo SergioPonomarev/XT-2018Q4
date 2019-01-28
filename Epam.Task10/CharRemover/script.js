f6905ca7309cce6b6f47da55ccd567d497809a23
Tahmasib <sergiopono@gmail.com>
2019-01-28 17:47:01 +0400

    Task10 CharRemover implementation.

---
+++ Epam.Task10/CharRemover/script.js
@@ -0,0 +1,44 @@
+var separators = ["?", "!", ":", ";", ",", ".", " ", "\t"],
+    str, 
+    arr, 
+    letters = {}, 
+    start = 0, 
+    words = [], 
+    result;
+
+document.getElementById('submitButton').onclick = function () {
+    str = document.getElementById('inputString').value;
+    getWordsFromInput(str);
+    findSameLetters();
+    getResultString();
+    document.getElementById('outputString').value = result;
+    event.preventDefault();
+}
+
+function getWordsFromInput(string) {
+    arr = string.split('');
+    arr.forEach(function (letter, i) {
+        if (separators.indexOf(letter) != -1) {
+            words.push(str.substr(start, i - start));
+            start = i + 1;
+        }
+    });
+
+    words.push(str.substr(start));
+}
+
+function findSameLetters() {
+    words.forEach(function (word) {
+        word.split('').forEach(function (letter, i) {
+            if (!letters[letter] && word.indexOf(letter, i + 1) != -1) {
+                letters[letter] = true;
+            }
+        });
+    });
+}
+
+function getResultString() {
+    result = arr.filter(function (check) {
+        return !letters[check];
+    }).join('');
+}
